b8a4e1027661b1ea2527ec41166bfcf8c5d8a43a
Xavier <xavier-tan@hotmail.sg>
2026-07-10 02:06:45 -0700

    Introduced ColorTechnique to hold vert + frag shader for rendering particles after each compute shader dispatch

---
+++ ColorTechnique.cpp
@@ -0,0 +1,29 @@
+#include "ColorTechnique.h"
+
+ColorTechnique::ColorTechnique() {
+}
+
+void ColorTechnique::setWVP(const glm::mat4x4& VP)
+{
+	glUniformMatrix4fv(m_WVPLoc, 1, /*Transpose*/GL_FALSE, glm::value_ptr(VP));
+}
+
+void ColorTechnique::setColor(const glm::vec4& c)
+{
+	glUniform4f(m_colorLoc, c.r, c.g, c.b, c.a);
+}
+
+bool ColorTechnique::Init() {
+	const char* placeHolderVShader = nullptr; 
+	const char* placeHolderFShader = nullptr;
+
+	// Create shader prog + compile, and link vert and frag shader 
+	if (!AddShader(GL_VERTEX_SHADER, placeHolderVShader, placeHolderFShader)) {
+		return false;
+	}
+	
+	m_WVPLoc = GetUniformLocation("WVPMatrix"); // or gWVP in the OG case
+	m_colorLoc = GetUniformLocation("Color"); // or gColor in OG case
+
+	return true;
+}
